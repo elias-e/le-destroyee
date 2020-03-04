1e2842a57ba380c9dc0c59dce2028bc45745c914
elias.eckermann <elias.eckermann@kk-gym-edv2-16.gym.kk.local>
2020-03-04 18:07:10 +0100

    init

---
+++ src/basic/testdest.java
@@ -0,0 +1,14 @@
+package basic;
+
+import lejos.hardware.motor.Motor;
+
+public class testdest {
+
+	public static void main(String[] args) {
+		Motor.A.forward();
+		Motor.B.forward();
+		Motor.C.forward();
+
+	}
+
+}
